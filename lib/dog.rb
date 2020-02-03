d7ef3e24672439b13b218e61588324a15cdd17e3
Ola Enol <eniolar10@gmail.com>
2020-02-03 03:38:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,9 +7,6 @@ class Dog
   end
 end
  
-fido = Dog.new
-fido.bark #> "Woof!"
-#By defining #bark within the Dog class, bark becomes a method of all instances of Dogs. If we make more dogs, they can all bark.
 
 class Dog
   def sit
