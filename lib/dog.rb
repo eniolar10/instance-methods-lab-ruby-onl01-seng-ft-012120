77104b4bc40a9784d5edc4cc12aeb46b91575f5a
Ola Enol <eniolar10@gmail.com>
2020-02-03 03:34:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,18 @@
 class Dog
+  # Class body
+ 
+  # Instance Method Definition
+  def bark
+    puts "Woof!"
+  end
+end
+ 
+fido = Dog.new
+fido.bark #> "Woof!"
+By defining #bark within the Dog class, bark becomes a method of all instances of Dogs. If we make more dogs, they can all bark.
+
+class Dog
+  def bark
+    puts "Woof!"
+  end
 end
